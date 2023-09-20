0a99b30dee614c3b4c70e4240fe0a44727f04b68
Ushinnary <kartushin.alexander97@gmail.com>
2023-09-20 07:37:13 +0200

    [Array][Rust] append drain clear is_empty split +

--- src/array/array.rust.ts
+++ src/array/array.rust.ts
@@ -5,6 +5,22 @@ declare global {
 		first(): T | undefined;
 		/// Returns the last element of the array.
 		last(): T | undefined;
+		/// Appends the elements of the given array to the end of this array. The given array is emptied.
+		append(items: T[]): T[];
+		/// Removes the elements from the array and return them as a new array.
+		drain(fromIdx: number, toIdx: number): T[];
+		/// Removes all elements from the array.
+		clear(): void;
+		/// Returns true if the array is empty.
+		is_empty(): boolean;
+		/// Removes all but the first n elements from the array.
+		split_off(at: number): T[];
+		/// Resizes the array in-place so that len is the new length.
+		resize_with(len: number, f?: () => T): void;
+		/// Resizes the array in-place so that len is the new length.
+		resize(len: number, value?: T): void;
+		/// Removes consecutive repeated elements in the array.
+		dedup(): void;
 	}
 }
 
@@ -26,6 +42,107 @@ if (!Array.prototype.last) {
 		configurable: false,
 		value: function () {
 			return this.length > 0 ? this[this.length - 1] : undefined;
+		},
+	});
+}
+if (!Array.prototype.append) {
+	Object.defineProperty(Array.prototype, "append", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function (others) {
+			while (others.length > 0) {
+				this.push(others.shift());
+			}
+		},
+	});
+}
+if (!Array.prototype.drain) {
+	Object.defineProperty(Array.prototype, "drain", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function (fromIdx, toIdx) {
+			return this.splice(fromIdx, toIdx - fromIdx + 1);
+		},
+	});
+}
+if (!Array.prototype.clear) {
+	Object.defineProperty(Array.prototype, "clear", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function () {
+			this.length = 0;
+		},
+	});
+}
+if (!Array.prototype.is_empty) {
+	Object.defineProperty(Array.prototype, "is_empty", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function () {
+			return this.length === 0;
+		},
+	});
+}
+if (!Array.prototype.split_off) {
+	Object.defineProperty(Array.prototype, "split_off", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function (idx) {
+			return this.splice(idx, this.length - idx);
+		},
+	});
+}
+if (!Array.prototype.resize_with) {
+	Object.defineProperty(Array.prototype, "resize_with", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function (length, f) {
+			if (length < this.length) {
+				this.splice(length, this.length - length);
+			} else {
+				while (this.length < length) {
+					this.push(f?.() ?? null);
+				}
+			}
+		},
+	});
+}
+if (!Array.prototype.resize) {
+	Object.defineProperty(Array.prototype, "resize", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function (length, value) {
+			if (length < this.length) {
+				this.splice(length, this.length - length);
+			} else {
+				while (this.length < length) {
+					this.push(value ?? null);
+				}
+			}
+		},
+	});
+}
+if (!Array.prototype.dedup) {
+	Object.defineProperty(Array.prototype, "dedup", {
+		enumerable: false,
+		writable: false,
+		configurable: false,
+		value: function () {
+			for (let i = 0; i < this.length; i++) {
+				if (this[i] === this[i - 1]) {
+					this.splice(i, 1);
+					i--;
+				}
+			}
+
+			return this;
 		},
 	});
 }
--- tests/array/array.rust.test.ts
+++ tests/array/array.rust.test.ts
@@ -20,4 +20,86 @@ describe("Array rust", () => {
 			expect([].last()).toBeUndefined();
 		});
 	});
+
+	describe("#append()", () => {
+		it("should append the elements of the given array to the end of this array", () => {
+			const array = [1, 2, 3];
+			const toEmptyArr = [4, 5, 6];
+			array.append(toEmptyArr);
+			expect(array).toEqual([1, 2, 3, 4, 5, 6]);
+			expect(toEmptyArr).toEqual([]);
+		});
+	});
+
+	describe("#drain()", () => {
+		it("should remove the elements from the array and return them as a new array", () => {
+			const array = [1, 2, 3, 4, 5, 6];
+			const drained = array.drain(1, 4);
+			expect(array).toEqual([1, 6]);
+			expect(drained).toEqual([2, 3, 4, 5]);
+		});
+	});
+
+	describe("#clear()", () => {
+		it("should remove all elements from the array", () => {
+			const array = [1, 2, 3, 4, 5, 6];
+			array.clear();
+			expect(array).toEqual([]);
+		});
+	});
+
+	describe("#is_empty()", () => {
+		it("should return true if the array is empty", () => {
+			expect([].is_empty()).toBe(true);
+		});
+
+		it("should return false if the array is not empty", () => {
+			expect([1, 2, 3].is_empty()).toBe(false);
+		});
+	});
+
+	describe("#split_off()", () => {
+		it("should remove all elements after the given index and return them as a new array", () => {
+			const array = [1, 2, 3, 4, 5, 6];
+			const split = array.split_off(3);
+			expect(array).toEqual([1, 2, 3]);
+			expect(split).toEqual([4, 5, 6]);
+		});
+	});
+
+	describe("#resize_with()", () => {
+		it("should resize the array to the given length", () => {
+			const array = [1, 2, 3, 4, 5, 6];
+			array.resize_with(3);
+			expect(array).toEqual([1, 2, 3]);
+		});
+
+		it("should fill the array with the result of the given function", () => {
+			const array = [1, 2, 3, 4, 5, 6];
+			array.resize_with(8, () => 0);
+			expect(array).toEqual([1, 2, 3, 4, 5, 6, 0, 0]);
+		});
+	});
+
+	describe("#resize()", () => {
+		it("should resize the array to the given length", () => {
+			const array = [1, 2, 3, 4, 5, 6];
+			array.resize(3);
+			expect(array).toEqual([1, 2, 3]);
+		});
+
+		it("should fill the array with the given value", () => {
+			const array = [1, 2, 3, 4, 5, 6];
+			array.resize(8, 0);
+			expect(array).toEqual([1, 2, 3, 4, 5, 6, 0, 0]);
+		});
+	});
+
+	describe("#dedup()", () => {
+		it("should remove consecutive duplicate elements from the array", () => {
+			const array = [1, 1, 2, 3, 3, 3, 4, 5, 5, 2];
+			array.dedup();
+			expect(array).toEqual([1, 2, 3, 4, 5, 2]);
+		});
+	});
 });
