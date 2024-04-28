4264c108b7efaadfca14426377c88b831443fe4d
Ushinnary <kartushin.alexander97@gmail.com>
2024-04-28 09:53:41 +0200

    Added Sum method
    Creating new class to let declare some methods specific to some type

--- src/array/array.linq.lazy.ts
+++ src/array/array.linq.lazy.ts
@@ -1,89 +1,95 @@
 enum ActionType {
-	Filter = 0,
-	Map = 1,
-	First = 2,
+  Filter = 0,
+  Map = 1,
+  First = 2,
 }
 
 export class IEnumerable<T> {
-	private stack: Array<[ActionType, (...args: any[]) => any]> = [];
-	private items: T[];
+  private stack: Array<[ActionType, (...args: any[]) => any]> = [];
+  private items: T[];
 
-	constructor(items: T[] = []) {
-		this.items = items ?? [];
-	}
+  constructor(
+    items: T[] = [],
+    stack?: Array<[ActionType, (...args: any[]) => any]>,
+  ) {
+    this.items = items ?? [];
+    this.stack = stack ?? [];
+  }
 
-	public Where(predicate: (item: T) => boolean = () => true): IEnumerable<T> {
-		this.stack.push([ActionType.Filter, predicate]);
+  public Where(predicate: (item: T) => boolean = () => true): IEnumerable<T> {
+    this.stack.push([ActionType.Filter, predicate]);
 
-		return this;
-	}
+    return this;
+  }
 
-	public Select<U>(predicate: (item: T) => U): IEnumerable<T> {
-		this.stack.push([ActionType.Map, predicate]);
+  public Select<U>(predicate: (item: T) => U): IEnumerable<U> {
+    this.stack.push([ActionType.Map, predicate]);
 
-		return this;
-	}
+    return new IEnumerable(this.items as unknown as U[], this.stack);
+  }
 
-	public First(predicate?: (item: T) => boolean): T | undefined {
-		const callback = predicate ?? ((item) => !!item);
+  public Sum(this: IEnumerable<number>): number {
+    return this.ToArray().reduce((total, curr) => total + curr, 0);
+  }
 
-		this.stack.push([ActionType.First, callback]);
+  public First(predicate?: (item: T) => boolean): T | undefined {
+    this.stack.push([ActionType.First, predicate ?? ((item) => !!item)]);
 
-		return this.ToArray()[0];
-	}
+    return this.ToArray()[0];
+  }
 
-	public ToArray(): T[] {
-		const result: T[] = [];
+  public ToArray(): T[] {
+    const result: T[] = [];
 
-		let foundNeeded = false;
+    let foundNeeded = false;
 
-		for (const item of this.items) {
-			let newItem = item;
+    for (const item of this.items) {
+      let newItem = item;
 
-			let shouldContinue = false;
+      let shouldContinue = false;
 
-			for (const [action, callback] of this.stack) {
-				let shouldBreak = false;
+      for (const [action, callback] of this.stack) {
+        let shouldBreak = false;
 
-				switch (action) {
-					case ActionType.Filter: {
-						shouldBreak ||= !callback(newItem);
-						shouldContinue ||= shouldBreak;
+        switch (action) {
+          case ActionType.Filter: {
+            shouldBreak ||= !callback(newItem);
+            shouldContinue ||= shouldBreak;
 
-						break;
-					}
-					case ActionType.Map: {
-						newItem = callback(newItem);
+            break;
+          }
+          case ActionType.Map: {
+            newItem = callback(newItem);
 
-						break;
-					}
-					case ActionType.First: {
-						const isValid = callback(newItem);
+            break;
+          }
+          case ActionType.First: {
+            const isValid = callback(newItem);
 
-						shouldBreak = true;
-						shouldContinue = !isValid;
-						foundNeeded ||= isValid;
+            shouldBreak = true;
+            shouldContinue = !isValid;
+            foundNeeded ||= isValid;
 
-						break;
-					}
-				}
+            break;
+          }
+        }
 
-				if (shouldBreak) {
-					break;
-				}
-			}
+        if (shouldBreak) {
+          break;
+        }
+      }
 
-			if (shouldContinue) {
-				continue;
-			}
+      if (shouldContinue) {
+        continue;
+      }
 
-			result.push(newItem);
+      result.push(newItem);
 
-			if (foundNeeded) {
-				break;
-			}
-		}
+      if (foundNeeded) {
+        break;
+      }
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
